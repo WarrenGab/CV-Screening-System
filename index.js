6081f2c7478372abb6a98959cf1a19aa032fa6f0
WarrenGab <warren.gabriel6@gmail.com>
2023-06-03 18:31:40 +0700

    Update on CORS middleware

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
